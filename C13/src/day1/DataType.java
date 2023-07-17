c7dfeee89f387a3f9e0d7cc1fab0a07347fa0051
Afsar <Afsar@afsar-tuf>
2023-07-17 21:01:58 +0530

    commit

---
+++ C13/src/day1/DataType.java
@@ -0,0 +1,38 @@
+package day1;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		//int,float,char,string,double,boolean,long etc,.
+		
+		//Primitive Datatypes:
+		/*
+		 * It can hold only one value at a time
+		 */
+		int a=1;
+		char b= 'a';
+		float c= 11.6f;
+		double d= 20.03;
+		boolean e= true;
+		
+		System.out.println(a);
+		System.out.println("Integer is : "+a);
+		System.out.println("Integer is : "+a + " Charcter is : "+b);
+		System.out.println("Double is "+d);
+		System.out.println("Boolean is : "+e);
+		
+		//Non Primitive Datatypes:
+		/*
+		 * It can hold more values
+		 */
+		// It takes more memory bytes
+		String aa = "Hi Hello";
+		String bb = "10";
+		String cc = "true";
+		String dd = "11.6";
+		String ee = "a";
+		
+
+	}
+
+}
