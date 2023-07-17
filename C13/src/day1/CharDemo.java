f0c2ce0ea2e71f5e0e91e6ac52659f4f9cd362e4
Afsar <Afsar@afsar-tuf>
2023-07-17 20:59:48 +0530

    commit

---
+++ C13/src/day1/CharDemo.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		char a = 67;
+		System.out.println(a);
+		
+		int b='a';
+		System.out.println(b);
+		
+		char c = '\u00A7';
+		
+		char d = '\u20AC';
+		
+		//char e = '\U+1F600';6
+		
+		System.out.println(c);
+		System.out.println(d);
+	
+	}
+
+}
